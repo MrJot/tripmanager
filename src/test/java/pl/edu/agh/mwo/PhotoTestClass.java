169c60619cbe508d5de2ab4040d0dfa45e87fdeb
Grzegorz Juszkiewicz <grzegorzjuszkiewicz@Grzegorzs-MacBook-Air.local>
2018-04-03 00:07:45 +0200

    TestPhotoClass is not working  part 3

--- src/test/java/pl/edu/agh/mwo/PhotoTestClass.java
+++ src/test/java/pl/edu/agh/mwo/PhotoTestClass.java
@@ -2,24 +2,21 @@ package pl.edu.agh.mwo;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class PhotoTestClass {
 	
-	Photo photo;
-	
-	@Before
-	public void setUp() {
-		photo=new Photo();
+	@Test
+	public void testPhotoComment() {
+		Photo photo = new Photo();
+		photo.setComment("Nice photo!");
+		assertEquals(photo.getComment(), "Nice photo!");
 	}
 	
 
-	@Test
-	public void testGetComment() {
-		photo.setComment("This is a very nice shot !");
-		assertEquals(photo.getComment(),"This is a very nice shot !" );
-	}
+	
+
+
 	
 
 	
